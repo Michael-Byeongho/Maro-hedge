94618a67ee8f48e0f52a9f087ddf44f4febb411f
Michael-Byeongho <lbyeongho3071@gmail.com>
2025-06-18 23:55:32 +0900

    Delete hedge_app_fixed_final_upload (1) directory

--- hedge_app_fixed_final_upload (1)/app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Hedge Dashboard", layout="wide")
-st.title("📈 실물/선물 포지션 & 매크로 대시보드")
-
-st.markdown("왼쪽 사이드바에서 기능을 선택하세요.\n\n- 📊 매크로 지표 모니터링\n- 📦 실물 헷지 분석")
